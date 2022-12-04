b47acadd5ea903704b2f0c711ccc657a3d78dba9
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-04 18:36:38 -0300

    Remove code innecesary. Change name module.

--- projects/tots/core/src/lib/tots-core.module.ts
+++ projects/tots/core/src/lib/tots-core.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CoreComponent } from './core.component';
 
 
 
 @NgModule({
   declarations: [
-    CoreComponent
+    
   ],
   imports: [
   ],
   exports: [
-    CoreComponent
+    
   ]
 })
-export class CoreModule { }
+export class TotsCoreModule { }
--- projects/tots/core/src/public-api.ts
+++ projects/tots/core/src/public-api.ts
@@ -2,6 +2,7 @@
  * Public API Surface of core
  */
 
-export * from './lib/core.service';
-export * from './lib/core.component';
-export * from './lib/core.module';
+/**
+ * Modules
+ */
+export * from './lib/tots-core.module';
