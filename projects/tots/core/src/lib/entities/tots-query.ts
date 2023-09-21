11a881c2fd8c6d54fe0c3f2c0bc585bd5201e738
Matias Camiletti <matias.camiletti@gmail.com>
2023-09-21 11:10:35 -0300

    Add extra params in tots query.

--- projects/tots/core/src/lib/entities/tots-query.ts
+++ projects/tots/core/src/lib/entities/tots-query.ts
@@ -1,36 +1,40 @@
 export class TotsQuery {
     /**
-     * 
+     *
      */
     page = 1;
     /**
-     * 
+     *
      */
     per_page = 50;
     /**
-     * 
+     *
      */
     wheres: Array<any> = [];
     /**
-     * 
+     *
      */
     groups: Array<any> = [];
     /**
-     * 
+     *
      */
     sums: Array<any> = [];
     /**
-     * 
+     *
      */
     withs: Array<any> = [];
     /**
-     * 
+     *
      */
     orders: Array<any> = [];
+    /**
+     *
+     */
+    extraParams: any;
 
     /**
-     * 
-     * @param key 
+     *
+     * @param key
      * @param value
      */
      addWhereEqual(key: string, value: any) {
@@ -41,10 +45,10 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
-     * @param from 
-     * @param to 
+     *
+     * @param key
+     * @param from
+     * @param to
      */
      addWhereBetween(key: string, from: any, to: any) {
         this.wheres.push({
@@ -55,10 +59,10 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
-     * @param from 
-     * @param to 
+     *
+     * @param key
+     * @param from
+     * @param to
      */
      addWhereBetweenDate(key: string, from: any, to: any) {
         this.wheres.push({
@@ -69,9 +73,9 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
-     * @param value 
+     *
+     * @param key
+     * @param value
      */
      addWhereDate(key: string, value: string) {
         this.wheres.push({
@@ -81,9 +85,9 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
-     * @param value 
+     *
+     * @param key
+     * @param value
      */
     addWhereGreaterThan(key: string, value: any) {
         this.wheres.push({
@@ -93,9 +97,9 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
-     * @param values 
+     *
+     * @param key
+     * @param values
      */
     addWhereIn(key: string, values: any) {
         this.wheres.push({
@@ -105,9 +109,9 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
-     * @param value 
+     *
+     * @param key
+     * @param value
      */
     addWhereLessThan(key: string, value: any) {
         this.wheres.push({
@@ -117,9 +121,9 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
-     * @param value 
+     *
+     * @param key
+     * @param value
      */
      addWhereYear(key: string, value: any) {
         this.wheres.push({
@@ -129,9 +133,9 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
-     * @param value 
+     *
+     * @param key
+     * @param value
      */
      addWhereMonth(key: string, value: any) {
         this.wheres.push({
@@ -141,8 +145,8 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
+     *
+     * @param key
      * @param value 2020-01-01
      */
      addWhereWeek(key: string, value: any) {
@@ -153,8 +157,8 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
+     *
+     * @param key
      * @param value
      */
     addWhereLikes(keys: Array<string>, value: string) {
@@ -165,8 +169,8 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
+     *
+     * @param key
      */
     addWhereNull(key: string) {
         this.wheres.push({
@@ -175,97 +179,97 @@ export class TotsQuery {
         })
     }
     /**
-     * 
+     *
      */
     removeAllWhereLikes() {
         this.removeWheresByType('likes');
     }
     /**
-     * 
+     *
      */
     removeAllWhereIn() {
         this.removeWheresByType('in');
     }
     /**
-     * 
+     *
      */
     removeAllWhereBetween() {
         this.removeWheresByType('between');
     }
     /**
-     * 
+     *
      */
     removeAllWhereBetweenDate() {
         this.removeWheresByType('between_date');
     }
     /**
-     * 
+     *
      */
     removeAllWhereEqual() {
         this.removeWheresByType('equal');
     }
     /**
-     * 
-     * @param type 
+     *
+     * @param type
      */
     removeWheresByType(type: string) {
         this.wheres = this.wheres.filter(item => item.type != type);
     }
     /**
-     * 
+     *
      */
     resetWheres() {
         this.wheres = [];
     }
     /**
-     * 
+     *
      */
     cleanWheres() {
         this.resetWheres();
     }
     /**
-     * 
+     *
      */
     resetOrders() {
         this.orders = [];
     }
     /**
-     * 
+     *
      */
     cleanOrders() {
         this.resetOrders();
     }
     /**
-     * 
+     *
      */
     convertWheresToBase64() {
         let string = JSON.stringify({ wheres: this.wheres, orders: this.orders });
         return btoa(string);
     }
     /**
-     * 
-     * @param key 
+     *
+     * @param key
      */
     addGroupBy(key: string) {
         this.groups.push(key);
     }
     /**
-     * 
-     * @param key 
+     *
+     * @param key
      */
     addSum(key: string) {
         this.sums.push(key);
     }
     /**
-     * 
-     * @param key 
+     *
+     * @param key
      */
     addWith(key: string) {
         this.withs.push(key);
     }
     /**
-     * 
-     * @param key 
+     *
+     * @param key
      */
     addOrderAsc(key: string) {
         this.orders.push({
@@ -274,8 +278,8 @@ export class TotsQuery {
         })
     }
     /**
-     * 
-     * @param key 
+     *
+     * @param key
      */
     addOrderDesc(key: string) {
         this.orders.push({
@@ -284,17 +288,63 @@ export class TotsQuery {
         })
     }
     /**
+     *
+     * @param key
+     * @param value
+     */
+    addExtraParam(key: string, value: any) {
+      if(this.extraParams == undefined){
+        this.extraParams = {};
+      }
+      this.extraParams[key] = value;
+    }
+    /**
+     *
+     * @param key
+     * @returns
+     */
+    removeExtraParam(key: string){
+      if(this.extraParams == undefined){
+        return;
+      }
+      delete this.extraParams[key];
+    }
+    /**
+     *
+     */
+    resetExtraParams(){
+      this.extraParams = undefined;
+    }
+
+    toStringExtraParams(){
+      let result = '';
+
+      if(this.extraParams == undefined){
+        return result;
+      }
+
+      // Itera a través de las propiedades del objeto
+      for (let key in this.extraParams) {
+        if (this.extraParams.hasOwnProperty(key)) {
+          let val = this.extraParams[key];
+          // Agrega la clave y el valor a la cadena
+          result += `${key}=${val}&`;
+        }
+      }
+      return result;
+    }
+    /**
      * Devuelve queryParams para el request HTTP
-     * @returns 
+     * @returns
      */
     toString() {
-        return 'page=' + this.page + '&per_page=' + this.per_page + '&withs=' + this.withs.join(',') + '&groups=' + this.groups.join(',') + '&sums=' + this.sums.join(',') + '&filters=' + this.convertWheresToBase64() + '&perPage=' + this.per_page;
+        return 'page=' + this.page + '&per_page=' + this.per_page + '&withs=' + this.withs.join(',') + '&groups=' + this.groups.join(',') + '&sums=' + this.sums.join(',') + '&filters=' + this.convertWheresToBase64() + '&perPage=' + this.per_page + '&' + this.toStringExtraParams();
     }
     /**
      * Devuelve queryParams para el request HTTP para poder copiarse
-     * @returns 
+     * @returns
      */
     toStringTest() {
-        return 'page=' + this.page + '&per_page=' + this.per_page + '&withs=' + this.withs.join(',') + '&groups=' + this.groups.join(',') + '&sums=' + this.sums.join(',') + '&filtersString=' + JSON.stringify({ wheres: this.wheres, orders: this.orders }) + '&perPage=' + this.per_page;
+        return 'page=' + this.page + '&per_page=' + this.per_page + '&withs=' + this.withs.join(',') + '&groups=' + this.groups.join(',') + '&sums=' + this.sums.join(',') + '&filtersString=' + JSON.stringify({ wheres: this.wheres, orders: this.orders }) + '&perPage=' + this.per_page + '&' + this.toStringExtraParams();
     }
 }
